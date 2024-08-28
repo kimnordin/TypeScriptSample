66b5ad69154f8c77570361a60642016bd41b5a97
Kim <nordinsane@gmail.com>
2024-08-28 14:11:19 +0200

    Add Input Counter Value Field

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,20 +1,43 @@
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { incremented, decremented, selectCount } from './counterSlice';
 import './Counter.css';
 
 export function Counter() {
+  const [inputValue, setInputValue] = useState<string>("");
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleAddInputValue = () => {
+    const numericalInput = parseFloat(inputValue);
+    if (!isNaN(numericalInput)) {
+      dispatch(incremented(numericalInput));
+      setInputValue("");
+    }
+  };
 
   return (
     <div className="counter">
       <h2>Counter</h2>
       <div className="countDisplay">{count}</div>
-      <div className="buttons">
-        <button onClick={() => dispatch(decremented(10))}>-10</button>
-        <button onClick={() => dispatch(decremented(1))}>-</button>
-        <button onClick={() => dispatch(incremented(1))}>+</button>
-        <button onClick={() => dispatch(incremented(10))}>+10</button>
+      <div className="buttonsWrapper">
+        <button className="counterButton" onClick={() => dispatch(decremented(10))}>-10</button>
+        <button className="counterButton" onClick={() => dispatch(decremented(1))}>-</button>
+        <button className="counterButton" onClick={() => dispatch(incremented(1))}>+</button>
+        <button className="counterButton" onClick={() => dispatch(incremented(10))}>+10</button>
+      </div>
+      <div className="inputWrapper">
+        <input
+          type="number"
+          placeholder="Enter a Value"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button className="counterButton" onClick={handleAddInputValue}>Add Input Value</button>
       </div>
     </div>
   );
